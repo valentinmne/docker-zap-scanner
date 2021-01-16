bcb89ff608b2984c206804b20bfe7cb08595d494
Theo Massard <theo@particule.io>
2021-01-16 19:42:04 +0100

    Reindent script

--- scanner.sh
+++ scanner.sh
@@ -9,47 +9,46 @@ LIGHTRED='\033[1;31m'
 
 if [ "$1" == "http" ]
 then
-baseurl="http://"
+    baseurl="http://"
 else
-baseurl="https://"
+    baseurl="https://"
 fi
 
 if [ "$2" == "" ]
 then
-filename='urls.txt'
+    filename='urls.txt'
 else
-filename="$2"
+    filename="$2"
 fi
 
 
 if [ "$2" == "" ]
 then
-delivery='/var/www/html'
+    delivery='/var/www/html'
 else
-delivery="$3"
+    delivery="$3"
 fi
 
-
-while read line; do
-# reading each line
-echo -e "${GREEN}Scanning $line${NOCOLOR}"
-echo -e "${YELLOW}Creating directory : $line/$(date +%Y-%m-%d)${NOCOLOR}"
-echo ""
-mkdir -p $delivery$line/$(date +%Y-%m-%d)
-cd $delivery$line/$(date +%Y-%m-%d)
-chmod 777 -R $delivery
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-full-scan.py -m 10 -t $baseurl$line  -g generated_file.conf -r shiny_report.html
-echo ""
-echo -e "${YELLOW}The report is located at $(pwd)${NOCOLOR}"
-cd ../..
-echo -e "${LIGHTGREEN}Scan of $baseurl$line Complete.${NOCOLOR}"
 
 user_interrupt(){
-        echo -e "\n\nKeyboard Interrupt detected."
-        sleep 2
-        echo -e "${LIGHTRED}You pressed Ctrl + C, your scan could be not complete or corrupted${NOCOLOR}"
+    echo -e "\n\nKeyboard Interrupt detected."
+    sleep 2
+    echo -e "${LIGHTRED}You pressed Ctrl + C, your scan could be not complete or corrupted${NOCOLOR}"
 }
-
 trap user_interrupt SIGINT
-echo ""
+
+while read line; do
+    # reading each line
+    echo -e "${GREEN}Scanning $line${NOCOLOR}"
+    echo -e "${YELLOW}Creating directory : $line/$(date +%Y-%m-%d)${NOCOLOR}"
+    echo ""
+    mkdir -p $delivery$line/$(date +%Y-%m-%d)
+    cd $delivery$line/$(date +%Y-%m-%d)
+    chmod 777 -R $delivery
+    docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-full-scan.py -m 10 -t $baseurl$line  -g generated_file.conf -r shiny_report.html
+    echo ""
+    echo -e "${YELLOW}The report is located at $(pwd)${NOCOLOR}"
+    cd ../..
+    echo -e "${LIGHTGREEN}Scan of $baseurl$line Complete.${NOCOLOR}"
+    echo ""
 done < $filename
