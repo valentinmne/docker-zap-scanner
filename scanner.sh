a59b3e571014291dd8df6cefb3388b6b638d6b27
Theo Bob Massard <tbobm@protonmail.com>
2022-01-01 17:33:18 +0100

    fix(out): order generated files by target/date
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- scanner.sh
+++ scanner.sh
@@ -8,27 +8,27 @@ LIGHTGREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 LIGHTRED='\033[1;31m'
 
-scheme="${1:-http}://"
+scheme="${1:-http}"
 filename="${2:-urls.txt}"
-delivery="${3:-./}"
+delivery="${3:-.}"
 
 echo "Using scheme=$scheme filename=$filename delivery=$delivery"
 
 docker run --name zap -d -v $(pwd):/zap/wrk/:rw -l zap --entrypoint sleep owasp/zap2docker-stable infinity
-while read line; do
-    currentDirectory="$delivery$line/$(date +%Y-%m-%d)"
-    echo -e "${GREEN}Scanning $line${NOCOLOR}"
+while read entry; do
+    currentDirectory="${delivery}/${entry}/$(date +%Y-%m-%d)"
+    echo -e "${GREEN}Scanning $entry${NOCOLOR}"
     echo -e "${YELLOW}Creating directory : ${currentDirectory}${NOCOLOR}"
     echo ""
     mkdir -p $currentDirectory
 
-    docker exec -i zap /zap/zap-full-scan.py -m 10 -t $scheme$line  -g generated_file.conf -r shiny_report.html
-    pushd $currentDirectory
-        chmod 777 -R $delivery
-        echo ""
-        echo -e "${YELLOW}The report is located at $(pwd)${NOCOLOR}"
-    popd
-    echo -e "${LIGHTGREEN}Scan of $scheme$line Complete.${NOCOLOR}"
+    docker exec -i zap /zap/zap-full-scan.py -m 10 -t $scheme://$entry \
+        -g "generated_file.conf" \
+        -r "shiny_report.html" || true
+    mv -v generated_file.conf shiny_report.html $currentDirectory
+    echo ""
+    echo -e "${YELLOW}The report is located at $(pwd)${NOCOLOR}"
+    echo -e "${LIGHTGREEN}Scan of $scheme$entry Complete.${NOCOLOR}"
     echo ""
 done < $filename
 
